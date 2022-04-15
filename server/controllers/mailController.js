5e465ed60cdfe41fb1559044a8c8b6e8c8156197
Omar <omabdelalim@voceracommunicatins.com>
2022-04-15 11:21:19 -0400

    the backend of contact works

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -1,3 +1,27 @@
+const nodemailer = require('nodemailer')
+
 exports.sendemail = async (req, res) => {
-    console.log('this is working: ', req.body)
+    const {first_name, last_name, email, content} = req.body
+
+    let mailTransporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "omarh5877@gmail.com",
+            pass: "ptthrosqnestbmtu"
+        }
+    })
+
+    let details = {
+        from: email,
+        to: "oabdela@iu.edu",
+        subject: `Message from ${email}`,
+        text: content
+    }
+
+    mailTransporter.sendMail(details, (error) => {
+        if(!error)
+            console.log("email was sent")
+        else
+            console.log("BIG ERROR: ", error)
+    })
 }
