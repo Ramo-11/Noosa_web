8d020d47daf654173aa26681a12588fd5673fb51
Omar Abdelalim <omarh5877@gmail.com>
2022-05-08 00:04:34 -0400

    success and fail messages work perfectly for contact page and sign up and log in

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = express.Router()
 
-const dbController = require('./controllers/dbController')
+const dbUserController = require('./controllers/dbUserController')
 const mailController = require('./controllers/mailController')
 
 /**
@@ -35,13 +35,13 @@ route.get('/signup_and_login', (req, res) => res.render('signup_and_login'))
 // route.get('/change_password', (req, res) => res.render('change_password'))
 
 // API Routes
-route.post('/api/signup', dbController.createUser)
-route.get('/api/signup', dbController.findUser)
-route.put('/api/signup/:id', dbController.updateUser)
-route.delete('/api/signup/:id', dbController.deleteUser)
+route.post('/api/signup', dbUserController.createUser)
+route.get('/api/signup', dbUserController.findUser)
+route.put('/api/signup/:id', dbUserController.updateUser)
+route.delete('/api/signup/:id', dbUserController.deleteUser)
 
-route.post('/api/login', dbController.loginUser)
-route.post('/api/change_password', dbController.change_password)
+route.post('/api/login', dbUserController.loginUser)
+route.post('/api/change_password', dbUserController.change_password)
 
 route.post('/api/sendemail', mailController.sendemail)
 
